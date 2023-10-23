bd30146caac04dbd67116f90a406337aa25d2a7c
Xue-Zheng <Xue.Zheng@lincolnuni.ac.nz>
2023-10-24 10:38:50 +1300

    update .gitignore file

--- connect.py
+++
@@ -1,7 +0,0 @@
-# These details are available on the first MySQL Workbench screen
-# Usually called 'Local Instance'
-dbuser = "root" # Your MySQL username - likely 'root'
-dbpass = "Clara521521" # ---- PUT YOUR PASSWORD HERE ----
-dbhost = "localhost" 
-dbport = "3306"
-dbname = "motorkhana"
